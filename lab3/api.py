0f489e2ee433b6773ee814147d698fe6a414c2d1
unknown <nguyentri2307@gmail.com>
2025-06-11 13:53:04 +0700

    update lab3

--- lab3/api.py
+++ lab3/api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from cipher.rsa import RSACipher
-
+from cipher.ecc import ECCCipher
+ 
 app = Flask(__name__)
 
 # RSA_CIPHER_ALGORITHM
@@ -64,6 +65,35 @@ def rsa_verify_signature():
     _, public_key = rsa_cipher.load_keys()
     signature = bytes.fromhex(signature_hex)
     is_verified = rsa_cipher.verify(message, signature, public_key)
+    return jsonify({'is_verified': is_verified})
+
+#ECC
+
+app = Flask(__name__)
+ecc_cipher = ECCCipher() # Khởi tạo đối tượng ECCipher
+
+@app.route('/api/ecc/generate_keys', methods=['GET'])
+def ecc_generate_keys():
+    ecc_cipher.generate_keys()
+    return jsonify({'message': 'Keys generated successfully'})
+
+@app.route('/api/ecc/sign', methods=['POST'])
+def ecc_sign_message():
+    data = request.json
+    message = data['message']
+    private_key, _ = ecc_cipher.load_keys() # Chỉ cần khóa riêng tư để ký
+    signature = ecc_cipher.sign(message, private_key)
+    signature_hex = signature.hex() # Chuyển chữ ký sang định dạng hex để truyền qua JSON
+    return jsonify({'signature': signature_hex})
+
+@app.route('/api/ecc/verify', methods=['POST'])
+def ecc_verify_signature():
+    data = request.json
+    message = data['message']
+    signature_hex = data['signature']
+    _, public_key = ecc_cipher.load_keys() # Chỉ cần khóa công khai để xác minh
+    signature = bytes.fromhex(signature_hex) # Chuyển chữ ký từ hex về bytes
+    is_verified = ecc_cipher.verify(message, signature, public_key)
     return jsonify({'is_verified': is_verified})
 
 # main function
